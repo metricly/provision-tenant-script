c185d3a0a7826a3a67bf397979582aa278bf2318
Jason Simpson <9949503+jfletchersimpson@users.noreply.github.com>
2018-07-12 19:32:35 -0400

    Update setupMetricly.sh
    Updated the script to pull out the Windows and Linux apiIDs.  Also made the json output file name dynamic.

--- setupMetricly.sh
+++ setupMetricly.sh
@@ -28,17 +28,28 @@ read -p 'Do you want to install the package type yes or no: ' installPackage
 # set the endpoint
 endpointAddress="https://app.netuitive.com/tenant/create"
 
-echo "Started"
+echo "Started.  Please wait new account being created..."
 #create new tenant account
-curl -X POST -u $username:$password -F 'email='$setEmail -F 'password='$passwordNew -F 'fullName='$setFirstName $setLastName -F 'firstName='$setFirstName -F 'lastName='$setLastName -F 'phone='$setPhone -F 'company='$setCompany $endpointAddress >> logFile.txt
-echo "A new tenant account has been created"
-
+curl -s -X POST -u $username:$password -F 'email='$setEmail -F 'password='$passwordNew -F 'fullName='$setFirstName $setLastName -F 'firstName='$setFirstName -F 'lastName='$setLastName -F 'phone='$setPhone -F 'company='$setCompany $endpointAddress >> logFile.txt
+echo "A new tenant account has been created!"
 
 #create new json file with all the datasource IDs for the new tenant account
 #wait 10 second before calling the API to let the provision complete
-echo "A new file called listDatasources.json is being created with IDs required to install the agents.  Wait 10 seconds..."
+echo "A new file called listDatasources_$setCompany.json is being created with IDs required to install the agents.  Wait 10 seconds..."
+echo "................................................................................................................................"
 sleep 10
-curl -X GET -u $setEmail:$passwordNew https://api.app.metricly.com/datasources?includeElements=true >> listDatasources.json
+curl -s -X GET -u $setEmail:$passwordNew https://api.app.metricly.com/datasources?includeElements=true >> listDatasources_$setCompany.json
+
+#Pull the Windows and Linux API ID from the listDatasources file
+IFS=$'\n'
+DSarray=($(cat listDatasources_$setCompany.json | sed "s%[\"]%%g;s%type:%\ntype:%g" | grep "type:" | sed "s%,properties.*apiId% apiId%;s%,.*%%;s%type:%%"))
+
+for DS in ${DSarray[@]}; do 
+if printf %s\\n $DS | grep -qF "WINDOWS" ;
+then echo "Copy the Windows apiID to install the agent: $DS"; fi
+if printf %s\\n $DS | grep -qF "INFRASTRUCTURE" ;
+then echo "Copy the Linux apiID to install the agent: $DS"; fi
+done
 
 
 if [ $installPackage == 'yes' ]; then
